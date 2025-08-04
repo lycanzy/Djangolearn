d37b002bc0c0cc65d597cb21f382b622927a2449
lycanzy <lycanzy@gmail.com>
2025-08-04 19:20:24 +0800

    changed to Question from Questions

--- Djangolearn/polls/models.py
+++ Djangolearn/polls/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.utils import timezone
 
 # Create your models here.
-class Questions(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
 
@@ -14,7 +14,7 @@ class Questions(models.Model):
         return self.question_text
     
 class Choice(models.Model):
-    question = models.ForeignKey(Questions, on_delete = models.CASCADE)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
     def __str__(self):
