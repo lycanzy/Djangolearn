b554535ede26908f9d83f30dea7c8df9a441e02c
lycanzy <lycanzy@gmail.com>
2025-08-04 18:30:51 +0800

    Updated views.py in polls app. Implemente render function that uses the index tempalte.

--- Djangolearn/polls/views.py
+++ Djangolearn/polls/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     response = f"You're looking at question {question_id}."
@@ -22,3 +20,4 @@ def results(request, question_id):
 def vote(request, question_id):
     response = f"You're voting on question {question_id}."
     return HttpResponse(response)
+
